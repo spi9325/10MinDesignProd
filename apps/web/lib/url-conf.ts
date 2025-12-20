f26771591c01a06f36c60287b1b55045c93eef59
spi9325 <sanketinamdar72@gmail.com>
2025-12-20 15:41:52 +0530

    adding url conf to fe

--- apps/web/lib/url-conf.ts
+++ apps/web/lib/url-conf.ts
@@ -2,7 +2,7 @@ export function getUrl() {
   if (typeof window === "undefined") {
     //for ssr url
     if (process.env.DOCKER == "true") {
-      return process.env.DOCKER_BACKEND_URL;
+      return process.env.SSR_BACKEND_URL;
     } else {
       return process.env.NEXT_PUBLIC_Backend_URL;
     }
